4261caf9ea6ee973bf699a16bfb9ab369631b86e
Alexander Timmins <alexanderstimmins@gmail.com>
2023-01-16 12:19:35 +0000

    completed task 3 awaiting review

---
+++ __tests__/app.test.js
@@ -0,0 +1,48 @@
+const seed = require('../db/seeds/seed');
+const app = require('../app');
+const request = require('supertest');
+const db = require('../db/connection');
+const data = require('../db/data/test-data');
+
+beforeEach(() => {
+  return seed(data);
+});
+afterAll(() => {
+  db.end();
+});
+
+describe('app.js', () => {
+  describe('/api', () => {
+    test('/api returns a string "Api ready to serve"', () => {
+      return request(app)
+        .get('/api')
+        .expect(200)
+        .then((response) => {
+          expect(response.text).toBe('Api ready to serve');
+        });
+    });
+  });
+
+  describe('/api/categories', () => {
+    test('returns a list of categories in an object key with array values', () => {
+      return request(app)
+        .get('/api/categories')
+        .expect(200)
+        .then((response) => {
+          console.log(response.body);
+          expect(typeof response.body).toBe('object');
+          expect(response.body.categories.length).toBeGreaterThan(0);
+          expect(
+            response.body.categories.forEach((catagory) => {
+              expect(catagory).toEqual(
+                expect.objectContaining({
+                  slug: expect.any(String),
+                  description: expect.any(String),
+                })
+              );
+            })
+          );
+        });
+    });
+  });
+});
---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { getCategories, standardResponse } = require('./controller');
+
+app.get('/api', standardResponse)
+app.get('/api/categories', getCategories);
+
+app.use((err, request, response, next) => {
+  if (err.status) {
+    response.status(err.status).send({ message: err.message });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, request, response, next) => {
+  if (err.code === '22P02') {
+    response.status(400).send({ message: 'Bad request' });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, request, response, next) => {
+  response
+    .status(500)
+    .send({ message: 'Internal server error (coders did a boo boo)' });
+});
+
+module.exports = app;
